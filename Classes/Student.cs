2fb99cb65f9c36e3a1bb0a313b5ac98e681c4c54
AlyonaUzahova <alyonauzhakova@gmail.com>
2025-10-04 17:38:54 +0500

    Разработанный класс Student. Чернышков

---
+++ Classes/Student.cs
@@ -0,0 +1,33 @@
+﻿namespace Constructions_Chenyshkov2.Classes
+{
+    public class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Surname { get; set; }
+        public bool Schoolarship { get; set; }
+        public int Course = 4;
+
+        public Student( string Firstname, string Lastname, string Surname) {
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Lastname = Lastname;
+        
+        }
+        public Student(string Firstname, string Lastname, string Surname, bool Schoolarship)
+            :this(Firstname, Lastname, Surname) {
+            this.Schoolarship = Schoolarship;   
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Schoolarship, int Course)
+            : this(Firstname, Lastname, Surname)
+        {
+            this.Course = Course;
+        }
+
+        public string GetFIO() {
+            return $"{Lastname}{Firstname}{Surname}";
+        }
+
+    }
+}
